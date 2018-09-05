591636bea2ed349cc40ee8ee580991f000d0a350
ariannagraphics <42961444+ariannagraphics@users.noreply.github.com>
2018-09-05 17:02:00 +0200

    Update form.js

--- form.js
+++ form.js
@@ -1,25 +1,25 @@
-// Fetching HTML Elements in Variables by ID.
+
 var x = document.getElementById("form_sample");
-var createform = document.createElement('form'); // Create New Element Form
-createform.setAttribute("action", ""); // Setting Action Attribute on Form
-createform.setAttribute("method", "post"); // Setting Method Attribute on Form
+var createform = document.createElement('form');
+createform.setAttribute("action", "");
+createform.setAttribute("method", "post");
 x.appendChild(createform);
 
-var heading = document.createElement('h2'); // Heading of Form
+var heading = document.createElement('h2');
 heading.innerHTML = "Contact Form ";
 createform.appendChild(heading);
 
-var line = document.createElement('hr'); // Giving Horizontal Row After Heading
+var line = document.createElement('hr');
 createform.appendChild(line);
 
 var linebreak = document.createElement('br');
 createform.appendChild(linebreak);
 
-var namelabel = document.createElement('label'); // Create Label for Name Field
-namelabel.innerHTML = "Your Name : "; // Set Field Labels
+var namelabel = document.createElement('label');
+namelabel.innerHTML = "Your Name : ";
 createform.appendChild(namelabel);
 
-var inputelement = document.createElement('input'); // Create Input Field for Name
+var inputelement = document.createElement('input');
 inputelement.setAttribute("type", "text");
 inputelement.setAttribute("name", "dname");
 createform.appendChild(inputelement);
@@ -27,11 +27,11 @@ createform.appendChild(inputelement);
 var linebreak = document.createElement('br');
 createform.appendChild(linebreak);
 
-var emaillabel = document.createElement('label'); // Create Label for E-mail Field
+var emaillabel = document.createElement('label');
 emaillabel.innerHTML = "Your Email : ";
 createform.appendChild(emaillabel);
 
-var emailelement = document.createElement('input'); // Create Input Field for E-mail
+var emailelement = document.createElement('input');
 emailelement.setAttribute("type", "text");
 emailelement.setAttribute("name", "demail");
 createform.appendChild(emailelement);
@@ -39,7 +39,7 @@ createform.appendChild(emailelement);
 var emailbreak = document.createElement('br');
 createform.appendChild(emailbreak);
 
-var messagelabel = document.createElement('label'); // Append Textarea
+var messagelabel = document.createElement('label');
 messagelabel.innerHTML = "Your Message : ";
 createform.appendChild(messagelabel);
 
@@ -50,7 +50,7 @@ createform.appendChild(texareaelement);
 var messagebreak = document.createElement('br');
 createform.appendChild(messagebreak);
 
-var submitelement = document.createElement('input'); // Append Submit Button
+var submitelement = document.createElement('input');
 submitelement.setAttribute("type", "submit");
 submitelement.setAttribute("name", "dsubmit");
 submitelement.setAttribute("value", "Submit");
